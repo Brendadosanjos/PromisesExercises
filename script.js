188f06e2e4d960dfbd03b1440b72dd92d3665fe2
brendadosanjos <brendakelly_oliveira@live.com>
2024-09-10 16:57:40 -0300

    fim do exercicio

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ myPromise3.then((data) => {
     console.log(`Aconteceu um erro! ${erro}`)
 })
 
-//Resolve várias promessas com .all (retorna apenas quando todas estiverem resolvidas, ou seja, só vai retornar quando o p2 estiver ok)
+//Resolve várias promessas com ALL (retorna apenas quando todas estiverem resolvidas, ou seja, só vai retornar quando o p2 estiver ok)
 
 const p1 = new Promise((resolve, reject) => {
     resolve('P1 Ok')
@@ -71,6 +71,43 @@ const resolveAll = Promise.all ([p1, p2, p3]).then((data) => {
     console.log(data);
     
 })
+//Resolve várias promessas com RACE (retorna a primeira que for resolvida)
 
-// Resolve várias promessas com race (retorna a primeiro que for resolvida)
+const p4 = new Promise((resolve, reject) => {
+    setTimeout(()=> {
+        resolve("P4 ok!")
+    },1000)
+})
 
+const p5 = new Promise((resolve, reject) => {
+    resolve("P5 ok!")
+})
+
+const p6 = new Promise((resolve, reject) => {
+    resolve("P6 ok!")
+})
+
+const revolveAllRace = Promise.race ([p4, p5, p6]).then((data) => {
+    console.log(data);
+    
+})
+
+//Fetch request na API do GITHUB
+//Fetch API
+
+const userName = "brendadosanjos"
+fetch(`https://api.github.com/users/${userName}`, {
+    method: 'GET',
+    headers: {
+        Accept: 'aplication/vnd.github.v3+json',
+    },
+}).then ((reponse) => {
+    console.log(typeof reponse)
+    console.log(reponse)
+    return reponse.json()
+}).then((data)=>{
+    console.log(data)
+    console.log(`O nome do usuário é ${data.name}`);
+}).catch((erro) => {
+    console.log('Houve algum erro');
+})
